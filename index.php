b224c8d49964e9dc42b309e245a6c4b0e984cf92
Popsana Barida <popsyjunior@gmail.com>
2018-02-01 17:34:08 +0100

    init pageant form

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title></title>
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+      <!-- Bootstrap CDN -->
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+      <!-- Optional theme -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+      <script  src="http://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+
+      <!-- Latest compiled and minified JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+      <!-- Paystack API -->
+      <script src="https://js.paystack.co/v1/paystack.js"></script>
+
+      <!-- Sweet Alert -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.8.6/sweetalert2.all.min.js"></script>
+
+      <!-- Moment JS -->
+      <script src="https://momentjs.com/downloads/moment-with-locales.js"></script>
+
+      <script src="js/vue.js"></script>
+      <!-- Vuejs -->
+   </head>
+   <body>
+      <div class="col-md-10 col-md-offset-1" style="padding: 30px 0">
+         <?php
+         require "reg-form.php";
+         ?>
+      </div>
+   </body>
+</html>
---
+++ reg-form.php
@@ -0,0 +1,286 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$referer_link = filter_input(INPUT_GET, "ref");
+?>
+
+<div id="root">
+   <form enctype="multipart/form-data" method="post" class="form reg-form" v-show="ready" @submit.prevent="navigate('forward')">
+      <div class="j-row">
+         <input type="hidden" name="referer_link" value="<?php echo $referer_link; ?>" />
+         <input type="hidden" name="valid_age" v-model="form.valid_age" />
+         <h4>Step {{step}}</h4>
+         <div v-show="inStep(1)" v-if="isFilled(1)">
+            <div class="col-md-6 form-group">
+               <label>First Name <span>*</span></label>
+               <div class="input">
+                  <input type="text" v-model="form.first_name" class="form-control" name="first_name" placeholder="First &amp; Last Name" required />
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Middle Name <span>*</span></label>
+               <div class="input">
+                  <input type="text" v-model="form.middle_name" class="form-control" name="middle_name" placeholder="Middle &amp; Last Name" required />
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Last Name <span>*</span></label>
+               <div class="input">
+                  <input type="text" v-model="form.last_name" class="form-control" name="last_name" placeholder="Last &amp; Last Name" required />
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Email <span>*</span></label>
+               <div class="input">
+                  <input type="email" v-model="form.email" class="form-control" name="pemail" placeholder="Enter Email Address" id="pf-email" value="popsyjunior@gmail.com" required />
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Date of Birth <span>*</span></label>
+               <div class="input">
+                  <input type="date" v-model="form.date_of_birth" @change="validateDate" class="form-control date-picker hasDatepicker" name="Date of Birth" placeholder="Enter Date of Birth" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Phone Number <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Phone Number" placeholder="Enter Phone Number" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Alternate Phone Number</label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Alternate Phone Number" placeholder="Enter Alternate Phone Number">
+               </div>
+            </div>
+
+            <div class="col-md-6 form-group">
+               <label>Nationality <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Nationality" placeholder="Enter Nationality" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Country of Residence <span>*</span></label>
+               <div class="input">
+                  <select class="form-control" name="Country of Residence" required="required" data-dpmaxz-eid="13">
+                     <option v-for="country in countries">{{country}}</option>
+                  </select>
+                  <i></i>
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>State <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="State" placeholder="Enter State" required="required">
+               </div>
+            </div>
+         </div>
+
+         <div v-show="inStep(2)" v-if="isFilled(2)">
+            <div class="col-md-6 form-group">
+               <label>Contact Address <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Contact Address" placeholder="Enter Contact Address" required="required">
+               </div>
+            </div>
+
+            <div class="col-md-6 form-group">
+               <label>Highest Educational Qualification <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Highest Educational Qualification" placeholder="Enter Highest Educational Qualification" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Higher Institution (Past or Present) <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Higher Institution (Past or Present)" placeholder="Enter Higher Institution (Past or Present)" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Name of Guarantor/Guardian <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Name of Guarantor/Guardian" placeholder="Enter Name of Guarantor/Guardian" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Phone Number of Guarantor/Guardian <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Phone Number of Guarantor/Guardian" placeholder="Enter Phone Number of Guarantor/Guardian" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Alternate Phone Number of Guarantor/Guardian</label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Alternate Phone Number of Guarantor/Guardian" placeholder="Enter Alternate Phone Number of Guarantor/Guardian">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Email Address of Guarantor/Guardian <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Email Address of Guarantor/Guardian" placeholder="Enter Email Address of Guarantor/Guardian" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Contact Address of Guarantor/Guardian <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="Contact Address of Guarantor/Guardian" placeholder="Enter Contact Address of Guarantor/Guardian" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>State <span>*</span></label>
+               <div class="input">
+                  <input type="text" class="form-control" name="State" placeholder="Enter State" required="required">
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Country of Residence <span>*</span></label>
+               <div class="input">
+                  <select class="form-control" name="Country of Residence" required="required" data-dpmaxz-eid="24">
+                     <option v-for="country in countries" :value="country">{{country}}</option>
+                  </select>
+                  <i></i>
+               </div>
+            </div>
+         </div>
+
+         <div v-show="inStep(3)" v-if="isFilled(3)">
+            <div class="col-md-6 form-group">
+               <label>Describe yourself. Share your work/educational experience expressing your passion/talents (150 word) <span>*</span></label>
+               <div class="input">
+                  <textarea type="text" class="form-control" name="work_experience" rows="3" placeholder="Enter Describe yourself. Share your work/educational experience expressing your passion/talents (150 word)" required="required"></textarea>
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Tell us something unique about you; something special you do or have done? (50 word) <span>*</span></label>
+               <div class="input">
+                  <textarea type="text" class="form-control" name="something_unique" rows="3" placeholder="Enter Tell us something unique about you; something special you do or have done? (50 word)" required="required"></textarea>
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Tell us something about you that can inspire others? (50 word) <span>*</span></label>
+               <div class="input">
+                  <textarea type="text" class="form-control" name="how_you_inspire_others" rows="3" placeholder="Enter Tell us something about you that can inspire others? (50 word)" required="required"></textarea>
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>Tell us some of your hobbies? (50 word) <span>*</span></label>
+               <div class="input">
+                  <textarea type="text" class="form-control" name="hobbies" rows="3" placeholder="Enter Tell us some of your hobbies? (50 word)" required="required"></textarea>
+               </div>
+            </div>
+            <div class="col-md-6 form-group">
+               <label>What are some of your personal values? (50 word) <span>*</span></label>
+               <div class="input">
+                  <textarea type="text" class="form-control" name="What are some of your personal values? (50 word)" rows="3" placeholder="Enter What are some of your personal values? (50 word)" required="required"></textarea>
+               </div>
+            </div>
+
+             <div class="col-md-6 form-group">
+               <label>Mention one person who has made great impact in your life? In what way? (100 word-limit) <span>*</span></label>
+               <div class="input">
+                  <textarea type="text" class="form-control" name="Mention one person who has made great impact in your life? In what way? (100 word-limit)" rows="3" placeholder="Enter Mention one person who has made great impact in your life? In what way? (100 word-limit)" required="required"></textarea>
+               </div>
+            </div>
+         </div>
+
+         <div v-show="inStep(4)" v-if="isFilled(4)">
+            <div class="col-md-6">
+               <div class="form-group">
+                  <label>What Leadership role have you played before? and how your community, school, or workplace has benefited from it.(150 word) <span>*</span></label>
+                  <div class="input">
+                     <textarea type="text" class="form-control" name="What Leadership role have you played before? and how your community, school, or workplace has benefited from it.(150 word)" rows="3" placeholder="Enter What Leadership role have you played before? and how your community, school, or workplace has benefited from it.(150 word)" required="required"></textarea>
+                  </div>
+               </div>
+               <div class="form-group">
+                  <label>What inspires you most about Africa (100 word) <span>*</span></label>
+                  <div class="input">
+                     <textarea type="text" class="form-control" name="What inspires you most about Africa (100 word)" rows="3" placeholder="Enter What inspires you most about Africa (100 word)" required="required"></textarea>
+                  </div>
+               </div>
+               <div class="form-group">
+                  <label>If all young women in the world looked up to you. In what way would you influence them? (200 word) <span>*</span></label>
+                  <div class="input">
+                     <textarea type="text" class="form-control" name="If all young women in the world looked up to you. In what way would you influence them? (200 word)" rows="3" placeholder="Enter If all young women in the world looked up to you. In what way would you influence them? (200 word)" required="required"></textarea>
+                  </div>
+               </div>
+
+               <div class="form-group">
+                  <label>Preferred State for Audition (Nigerian residents only) <span>*</span></label>
+                  <div class="input">
+                     <select class="form-control" name="Preferred State for Audition (Nigerian residents only)" required="required" data-dpmaxz-eid="25">
+                        <option value="Lagos State">Lagos State</option>
+                        <option value="Abuja">Abuja</option>
+                        <option value="Imo State">Imo State</option>
+                     </select>
+                  </div>
+               </div>
+            </div>
+
+            <div class="row">
+               <div class="col-md-6">
+                  <h4>Upload your Pictures</h4>
+               </div>
+
+               <div class="col-md-3" v-for="(img, i) in images">
+                  <div class="form-group">
+                     <img class="img img-rounded" :src="img.src" style="width:100%" />
+                  </div>
+
+                  <div class="form-group">
+                     <label>{{img.title}} (Max. File Size is 2MB</label>
+                     <div class="input  append-small-btn">
+                        <div class="file-button">
+                           <input type="file" name="File Name (Max. File Size is 2MB)" required @change="readURL($event,i+1)" />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <div v-show="inStep(5)" v-if="isFilled(5)">
+            <div class="row text-center">
+               <div class="col-md-12 form-group">
+                  <img src="https://awlo.org/missawlopageant/wp-content/plugins/payment-forms-for-paystack/public/../images/logos@2x.png" alt="cardlogos" class="paystack-cardlogos size-full wp-image-1096">
+               </div>
+               <div style="padding: 20px">
+                  <a href="#" class="btn btn-success btn-lg" v-on:click.prevent="payWithPaystack">Pay Now</a>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="col-md-12">
+         <div class="text-center">
+            <a href="#" @click="navigate('back')" class="btn btn-primary" v-show="step > 1">
+               <i class="fa fa-arrow-left"></i> Previous
+            </a>
+
+            &nbsp;
+
+            <button type="submit" class="btn btn-success" v-show="step < max_steps">
+               Next <i class="fa fa-arrow-right"></i>
+            </button>
+         </div>
+      </div>
+   </form>
+</div>
+
+<!-- Paystack -->
+<form >
+   <script src="https://js.paystack.co/v1/inline.js"></script>
+</form>
+
+<script>
+var payment = {
+   "accesscode": "<?php echo md5(md5(microtime) . md5(date())); ?>"
+}
+</script>
+
+<!-- Script for form -->
+<script type="text/javascript" src="js/reg-form.js?pg=<?php echo rand(1,99); ?>"></script>
